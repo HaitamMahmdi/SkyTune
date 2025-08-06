8c0ecd7b1b12375605221b5eab83ce91dd760477
unknown <haitammaharani@gmail.com>
2025-08-06 14:33:59 +0100

    add a slider to the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,7 +42,67 @@ function changText() {
   }
 }
 let changTextsetInterval = null;
+const slider = ref(null),
+  card = ref([null]);
+
+const getPageX = (event) =>
+  event.type.startsWith("touch") ? event.touches[0].pageX : event.pageX;
+
+let isSliding = ref(false),
+  startX,
+  startScrollLeft,
+  pos = ref(null);
+const slideStart = (event) => {
+  isSliding.value = true;
+  startX = getPageX(event);
+  startScrollLeft = slider.value.scrollLeft;
+};
+const sliding = (event) => {
+  if (!isSliding.value) return;
+  slider.value.scrollLeft = startScrollLeft - (getPageX(event) - startX);
+  pos.value = slider.value.scrollLeft;
+  console.log(`sliding`);
+};
+const infiniteScroll = () => {
+  if (slider.value.scrollLeft === 0) {
+    slider.value.scrollLeft =
+      slider.value.scrollWidth - 2 * slider.value.offsetWidth;
+    console.log(`you are at the end`);
+    console.log(slider.value.scrollLeft);
+  } else if (
+    Math.ceil(slider.value.scrollLeft) ===
+    slider.value.scrollWidth - slider.value.offsetWidth
+  ) {
+    console.log(slider.value.scrollLeft, slider.value.scrollWidth);
+
+    slider.value.scrollLeft = slider.value.offsetWidth;
+    console.log(`you are at the end but right`);
+    console.log(slider.value.scrollLeft);
+  }
+};
 onMounted(() => {
+  const sliderChildrens = [...slider.value.children].slice(1);
+  let cardPerview = Math.round(
+    slider.value.offsetWidth / sliderChildrens[0].offsetWidth
+  );
+  sliderChildrens
+    .slice(-cardPerview)
+    .reverse()
+    .forEach((card, index) => {
+      if (card) {
+        const clone = card.cloneNode(true);
+        clone.classList.add(`copy${index}`);
+        slider.value.prepend(clone);
+      }
+    });
+
+  sliderChildrens.slice(0, cardPerview).forEach((card, index) => {
+    if (card) {
+      const clone = card.cloneNode(true);
+      clone.classList.add(`copy0-${index}`);
+      slider.value.append(clone);
+    }
+  });
   changTextsetInterval = setInterval(changText, 1000);
 });
 onUnmounted(() => {
@@ -53,12 +113,13 @@ onUnmounted(() => {
   <section class="h-[100vh] relative">
     <img
       class="w-full h-full brightness-50"
-      src="/public/assets/bgImage.jpg"
-      alt=""
+      src="/assets/bgImage.jpg"
+      alt="Background music theme"
     />
     <div
-      class="absolute container flex flex-col justify-center top-3/6 left-3/6 transform w-full -translate-3/6"
+      class="absolute container flex flex-col justify-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full"
     >
+      <!-- Desktop animated text -->
       <div
         class="overflow-y-hidden md:block hidden md:h-10 lg:h-14 text-center text-2xl md:text-3xl lg:text-[2.5rem]"
       >
@@ -71,33 +132,50 @@ onUnmounted(() => {
           <h2>TURN UP THE MOMENT</h2>
         </div>
       </div>
+
+      <!-- Mobile text -->
       <h2
         ref="heroText"
         class="text-center text-[clamp(2rem,5vw,3rem)] hidden max-md:block"
       >
         LISTEN TO MUSIC
       </h2>
+
       <ButtonCom class="w-40 mx-auto mt-10">
         <template v-slot:contant>sign in</template>
       </ButtonCom>
     </div>
   </section>
-  <section class="h-[100vh] group relative flex">
+
+  <section
+    ref="slider"
+    @mousedown.capture="slideStart"
+    @mousemove="sliding"
+    @mouseup.capture="isSliding = false"
+    @mouseleave="isSliding = false"
+    @touchstart.capture="slideStart"
+    @touchmove="sliding"
+    @touchend.capture="isSliding = false"
+    @scroll="infiniteScroll"
+    class="h-fit group max-w-[90rem] scroll-auto grid grid-flow-col auto-cols-[17.75rem] overflow-hidden relative"
+  >
     <span
-      class="w-full h-full flex justify-center items-center group-hover:animate-disappear animate-reappear bg-[rgba(0,0,0,0.8)] z-10 absolute"
-      ><p class="text-[clamp(2.5rem,5vw,4rem)]">
-        fined and explore genres
-      </p></span
+      :style="{ left: `${pos}px` }"
+      class="w-full h-full max-md:!hidden flex justify-center items-center group-hover:animate-disappear animate-reappear bg-[rgba(0,0,0,0.8)] z-10 absolute"
     >
+      <p class="text-[clamp(2.5rem,5vw,4rem)]">Find and explore genres</p>
+    </span>
+
     <div
-      class="min-w-[calc(100%/5)] overflow-hidden relative"
       v-for="genre in genres"
-      :key="genre"
+      :key="genre.name"
+      class="min-w-[17.75rem] cursor-grab select-none h-[53rem] overflow-hidden relative"
     >
       <img
         class="h-full w-full peer"
         :src="`public/assets${genre.imageURL}`"
-        alt=""
+        :alt="`${genre.name} cover art`"
+        draggable="false"
       />
       <div
         class="absolute shadow-bigShdow bg-[rgba(4,3,3,0.72)] hover:animate-jumpIn peer-hover:animate-jumpIn animate-getOut flex-col items-center w-full"
