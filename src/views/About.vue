f0a823c5607d21ba1330aea19ebc47dbd09c3faa
unknown <haitammaharani@gmail.com>
2025-11-22 17:11:40 +0100

    ui:add contant to the about us page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,2 +1,100 @@
 <script setup></script>
-<template>about</template>
+<template>
+  <div class="container my-40 px-5">
+    <div class="flex justify-between max-lg:text-center flex-wrap items-center">
+      <div class="max-xl:max-w-2xl">
+        <h3 class="min-xl:text-5xl text-4xl uppercase">
+          <span class="text-main">Your Music</span>, Anywhere
+        </h3>
+        <p class="min-xl:max-w-[47.5rem] min-xl:text-2xl mt-9">
+          We built this app to give you instant access to your favorite
+          music—wherever you are Whether you're online or offline, enjoy
+          seamless playback with powerful features that make listening feel
+          effortless.
+        </p>
+      </div>
+      <img
+        class="max-xl:w-[300px] max-lg:hidden"
+        src="/public/assets/aboutUs5.png"
+        alt=""
+      />
+    </div>
+    <div
+      class="flex my-10 flex-wrap max-lg:text-center min-lg:flex-row-reverse justify-between items-center"
+    >
+      <div class="max-xl:max-w-2xl">
+        <h3 class="min-xl:text-5xl text-4xl uppercase">
+          <span class="text-main">Simple</span>,
+          <span class="text-main">Fast</span> &
+          <span class="text-main">Smart</span>
+        </h3>
+        <p class="min-xl:max-w-[47.5rem] min-xl:text-2xl mt-9">
+          Designed for ease and speed, our music player helps you discover,
+          organize, and enjoy songs without distractions. With smart
+          recommendations and easy playlist tools, your music is always a tap
+          away.
+        </p>
+      </div>
+      <img
+        class="max-xl:w-[300px] max-lg:hidden"
+        src="/public/assets/aboutUs4.png"
+        alt=""
+      />
+    </div>
+    <div class="flex flex-wrap justify-between max-lg:text-center items-center">
+      <div class="max-xl:max-w-2xl">
+        <h3 class="min-xl:text-5xl text-4xl uppercase">
+          Built for Every <span class="text-main">Listener</span>
+        </h3>
+        <p class="min-xl:max-w-[47.5rem] min-xl:text-2xl mt-9">
+          We built this app to give you instant access to your favorite
+          music—wherever you are Whether you're online or offline, enjoy
+          seamless playback with powerful features that make listening feel
+          effortless.
+        </p>
+      </div>
+      <img
+        class="max-xl:w-[300px] max-lg:hidden"
+        src="/public/assets/aboutUs3.png"
+        alt=""
+      />
+    </div>
+    <div
+      class="my-10 flex flex-wrap max-lg:text-center min-lg:flex-row-reverse justify-between items-center"
+    >
+      <div class="max-xl:max-w-2xl">
+        <h3 class="min-xl:text-5xl text-4xl uppercase">
+          For <span class="text-main">Music Lovers</span>
+        </h3>
+        <p class="min-xl:max-w-[47.5rem] min-xl:text-2xl mt-9">
+          This isn’t just another music app—it’s made by people who live and
+          breathe music. We believe in delivering a clean, beautiful, and
+          powerful player that puts users and artists first.
+        </p>
+      </div>
+      <img
+        class="max-xl:w-[300px] max-lg:hidden"
+        src="/public/assets/aboutUs2.png"
+        alt=""
+      />
+    </div>
+    <div class="flex flex-wrap max-lg:text-center justify-between items-center">
+      <div class="max-xl:max-w-2xl">
+        <h3 class="min-xl:text-5xl text-4xl uppercase">
+          Elevate Your Listening
+        </h3>
+        <p class="min-xl:max-w-[47.5rem] min-xl:text-2xl mt-9">
+          We built this app to give you instant access to your favorite
+          music—wherever you are Whether you're online or offline, enjoy
+          seamless playback with powerful features that make listening feel
+          effortless.
+        </p>
+      </div>
+      <img
+        class="max-xl:w-[300px] max-lg:hidden"
+        src="/public/assets/aboutUs1.png"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
